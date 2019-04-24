0d58898ab710d63d9cc103dd0997e026b8a7844e
WuZhe <zhe.wu@vipl.ict.ac.cn>
2019-04-24 19:32:21 +0800

    Update metric.py

--- metric.py
+++ metric.py
@@ -56,3 +56,100 @@ class cal_mae(object):
         return np.mean(self.prediction)
     
 
+class cal_sm(object):
+    # structure similarity
+    # Structure-measure: A new way to evaluate foreground maps (ICCV2017)
+    def __init__(self, alpha=0.5):
+        self.prediction = []
+        self.alpha = alpha
+
+    def update(self, pred, gt):
+        gt = gt > 0.5
+        score = self.cal(pred, gt)
+        self.prediction.append(score)
+
+    def show(self):
+        return np.mean(self.prediction)
+
+    def cal(self, pred, gt):
+        y = np.mean(gt)
+        if y == 0:
+            score = 1 - np.mean(pred)
+        elif y == 1:
+            score = np.mean(pred)
+        else:
+            score = self.alpha*self.object(pred, gt) + (1 - self.alpha) * self.region(pred, gt)
+        return score
+
+    def object(self, pred, gt):
+        fg = pred*gt
+        bg = (1-pred)*(1 - gt)
+
+        u = np.mean(gt)
+        return u*self.s_object(fg, gt) + (1-u)*self.s_object(bg, np.logical_not(gt))
+
+    def s_object(self, in1, in2):
+        x = np.mean(in1[in2])
+        sigma_x = np.std(in1[in2])
+        return 2*x / (pow(x, 2) + 1 + sigma_x + 1e-8)
+
+    def region(self, pred, gt):
+        [y, x] = ndimage.center_of_mass(gt)
+        y = int(round(y)) + 1
+        x = int(round(x)) + 1
+        [gt1, gt2, gt3, gt4, w1, w2, w3, w4] = self.divideGT(gt, x, y)
+        pred1, pred2, pred3, pred4 = self.dividePred(pred, x, y)
+
+        score1 = self.ssim(pred1, gt1)
+        score2 = self.ssim(pred2, gt2)
+        score3 = self.ssim(pred3, gt3)
+        score4 = self.ssim(pred4, gt4)
+
+        return w1*score1 + w2*score2 + w3*score3 + w4*score4
+
+    def divideGT(self, gt, x, y):
+        h, w = gt.shape
+        area = h*w
+        LT = gt[0:y, 0:x]
+        RT = gt[0:y, x:w]
+        LB = gt[y:h, 0:x]
+        RB = gt[y:h, x:w]
+
+        w1 = x*y / area
+        w2 = y*(w-x) / area
+        w3 = (h-y)*x / area
+        w4 = (h-y)*(w-x) / area
+
+        return LT, RT, LB, RB, w1, w2, w3, w4
+
+    def dividePred(self, pred, x, y):
+        h, w = pred.shape
+        LT = pred[0:y, 0:x]
+        RT = pred[0:y, x:w]
+        LB = pred[y:h, 0:x]
+        RB = pred[y:h, x:w]
+
+        return LT, RT, LB, RB
+
+    def ssim(self, in1, in2):
+        in2 = np.float32(in2)
+        h, w = in1.shape
+        N = h*w
+
+        x = np.mean(in1)
+        y = np.mean(in2)
+        sigma_x = np.var(in1)
+        sigma_y = np.var(in2)
+        sigma_xy = np.sum((in1-x)*(in2-y)) / (N-1)
+
+        alpha = 4 * x * y * sigma_xy
+        beta = (x*x + y*y)*(sigma_x + sigma_y)
+
+        if alpha != 0:
+            score = alpha / (beta + 1e-8)
+        elif alpha == 0 and beta == 0:
+            score = 1
+        else:
+            score = 0
+
+        return score
